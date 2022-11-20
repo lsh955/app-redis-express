12785398b56e28f2d84b53ce126b45b37e273efd
이승환 <lsh01087674117@gmail.com>
2022-11-20 19:02:14 +0900

    redis import 추가

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const redis = require('./../config/redis')
+const redisClient = redis.v4;
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
