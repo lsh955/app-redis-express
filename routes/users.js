e269d06d01a21b7a557ff07dfdebe71fe54dd1e9
이승환 <lsh01087674117@gmail.com>
2022-11-20 19:18:30 +0900

    기본 get, set 구성추가

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,22 @@ var router = express.Router();
 const redis = require('./../config/redis')
 const redisClient = redis.v4;
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+/**
+ * 데이터 가져오기
+ */
+router.get('/', async (req, res) => {
+  const result = await redisClient.get("test")
+
+  res.json(result)
+});
+
+/**
+ * 데이터 저장하기
+ */
+router.post('/', function(req, res, next) {
+  redisClient.set("test", "Hi")
+
+  res.json("등록완료")
 });
 
 module.exports = router;
